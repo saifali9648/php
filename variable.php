6f93a981e8f1b5e8bfc3ce279a3f1ef50bd83bab
saifali9648 <sali964876@gmail.com>
2023-05-02 22:35:23 +0530

    second php program

---
+++ variable.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  echo "A variable start with $ sign, followed by the name of the varible<br>";
+  echo "like<br>";
+  $name="saif <br>";
+  echo "name of the developer is $name <br>";
+  echo "A varible name must start with a letter or underscore charactor<br>";
+  echo "like";
+  $_name="saif";
+  echo "name of the boy is $_name<br>";
+  echo "A varible name can not start with a number <br>";
+  echo "like if you create like 5name this is the wrong way to declear a variable ";
+  echo "A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )";
+  echo "<br> in php varble names are case sensetive if you declear age or Age  both are two defferent variable name";
+  
+  ?>
+</body>
+</html>
