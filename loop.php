fa39331a478fbe5d8f0994e1c267389bc5e4f4be
saifali9648 <sali964876@gmail.com>
2023-05-03 23:12:47 +0530

    looping

---
+++ loop.php
@@ -0,0 +1,24 @@
+<?php
+echo "while loop program";
+$n=1;
+while ($n<=5) {
+    $a=1;
+    while ($a <=$n) {
+        echo $a;
+        $a++;
+    }
+    echo "<br>";
+    $n++;
+}
+echo"<br>";
+$n=1;
+while ($n<=5) {
+    $a=1;
+    while ($a <=$n) {
+        echo "*";
+        $a++;
+    }
+    echo "<br>";
+    $n++;
+}
+?>
